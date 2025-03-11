dafcd5360953ee79655e19676e593d77081939d4
eqfwcev123 <dohyeonee95@hotmail.com>
2025-03-12 07:50:47 +0800

    update websocket config

---
+++ src/main/java/com/enus/newsletter/ws/WebSocketSecurityConfig.java
@@ -0,0 +1,9 @@
+//package com.enus.newsletter.ws;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+//
+//@Configuration
+//@EnableWebSocketSecurity
+//public class WebSocketSecurityConfig {
+//}
--- src/main/java/com/enus/newsletter/ws/WebsocketConfig.java
+++ src/main/java/com/enus/newsletter/ws/WebsocketConfig.java
@@ -1,5 +1,6 @@
 package com.enus.newsletter.ws;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,15 +11,27 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        // client will subscribe to this endpoint
+        registry.enableStompBrokerRelay("/topic", "queue")
+                .setRelayHost("localhost")
+                .setRelayPort(61613) // default stop port
+                .setClientLogin("guest")
+                .setClientPasscode("guest")
+                .setSystemHeartbeatReceiveInterval(10000)
+                .setSystemHeartbeatSendInterval(10000);
+
+        // client will send messages to this endpoint
         registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // client will connect to this endpoint
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
-
 }
