941258f22c322384de039fde17012a0d0e0096bc
eqfwcev123 <dohyeonee95@hotmail.com>
2025-03-11 22:22:59 +0800

    .

---
+++ src/main/java/com/enus/newsletter/ws/model/Message.java
@@ -0,0 +1,17 @@
+package com.enus.newsletter.ws.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private MessageType type;
+    private String content;
+    private String sender;
+    private String sessionId;
+}
