4b928e2501ed7a2916bda104563dd736e91dde6a
eqfwcev123 <dohyeonee95@hotmail.com>
2025-03-14 21:50:35 +0800

    temp comment out

--- src/main/java/com/enus/newsletter/controller/ChatController.java
+++ src/main/java/com/enus/newsletter/controller/ChatController.java
@@ -1,28 +1,23 @@
 package com.enus.newsletter.controller;
 
-import com.enus.newsletter.model.dto.ChatMessage;
-import com.enus.newsletter.service.ChatService;
+import org.springframework.stereotype.Controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 
 @Controller
 @Slf4j(topic = "CHAT_CONTROLLER")
 public class ChatController {
-    private ChatService chatService;
+    // private ChatService chatService;
 
-    public ChatController(ChatService chatService) {
-        this.chatService = chatService;
-    }
+    // public ChatController(ChatService chatService) {
+    //     this.chatService = chatService;
+    // }
 
-    @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public ChatMessage chat(@Payload ChatMessage message) {
-        log.info("Received message: {}", message);
-        chatService.saveChatHistory(message);
-        return message;
-    }
+    // @MessageMapping("/chat")
+    // @SendTo("/topic/messages")
+    // public ChatMessage chat(@Payload ChatMessage message) {
+    //     log.info("Received message: {}", message);
+    //     chatService.saveChatHistory(message);
+    //     return message;
+    // }
 }
--- src/test/java/com/enus/newsletter/AuthControllerTest.java
+++ src/test/java/com/enus/newsletter/AuthControllerTest.java
@@ -1,45 +1,45 @@
-package com.enus.newsletter;
+// package com.enus.newsletter;
 
-import com.enus.newsletter.model.request.SignupRequest;
-import com.enus.newsletter.service.AuthService;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
+// import com.enus.newsletter.model.request.SignupRequest;
+// import com.enus.newsletter.service.AuthService;
+// import jakarta.transaction.Transactional;
+// import org.junit.jupiter.api.*;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.bean.override.mockito.MockitoBean;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.web.context.WebApplicationContext;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@Transactional
-public class AuthControllerTest {
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+// @Transactional
+// public class AuthControllerTest {
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+//     @Autowired
+//     private WebApplicationContext webApplicationContext;
 
-    @MockitoBean
-    private AuthService authService;
+//     @MockitoBean
+//     private AuthService authService;
 
-    private MockMvc mockMvc;
+//     private MockMvc mockMvc;
 
-    @BeforeEach
-    public void before() {
-        System.out.println("Test Before");
-    }
+//     @BeforeEach
+//     public void before() {
+//         System.out.println("Test Before");
+//     }
 
-    @Test
-    @DisplayName("Test Signup")
-    void testSignup() throws Exception {
-        SignupRequest request = SignupRequest.builder()
-                .firstName("robert")
-                .lastName("lee")
-                .username("admin1234")
-                .password("P@ssw0rd123")
-                .email("admin1234@test.com")
-                .build();
+//     @Test
+//     @DisplayName("Test Signup")
+//     void testSignup() throws Exception {
+//         SignupRequest request = SignupRequest.builder()
+//                 .firstName("robert")
+//                 .lastName("lee")
+//                 .username("admin1234")
+//                 .password("P@ssw0rd123")
+//                 .email("admin1234@test.com")
+//                 .build();
 
-    }
+//     }
 
-}
+// }
