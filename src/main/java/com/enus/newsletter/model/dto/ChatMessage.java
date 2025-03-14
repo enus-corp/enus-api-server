7954ba2eeef15609b0eee976b29180773d74dd7c
rlee <rlee@lscm.hk>
2025-03-14 17:19:14 +0800

    mesasge to be saved in redis

--- src/main/java/com/enus/newsletter/model/dto/ChatMessage.java
+++ src/main/java/com/enus/newsletter/model/dto/ChatMessage.java
@@ -1,5 +1,10 @@
 package com.enus.newsletter.model.dto;
 
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +12,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatMessage {
+@RedisHash("ChatMessage")
+public class ChatMessage implements Serializable {
+    @Id
+    private String id;
     private String sender;
     private String content;
     private String timestamp;
