5b2a93ae950539ebf65570dea551b908120d7b9e
eqfwcev123 <dohyeonee95@hotmail.com>
2025-03-15 15:53:22 +0800

    change JpaRepository to CRUD Repository and create RedisTemplate bean

--- src/main/java/com/enus/newsletter/controller/ChatController.java
+++ src/main/java/com/enus/newsletter/controller/ChatController.java
@@ -1,6 +1,12 @@
 package com.enus.newsletter.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
+import com.enus.newsletter.model.dto.ChatMessage;
+import com.enus.newsletter.service.ChatService;
 
 import lombok.extern.slf4j.Slf4j;
 
--- src/main/java/com/enus/newsletter/db/repository/imp/IRedisRepository.java
+++ src/main/java/com/enus/newsletter/db/repository/imp/IRedisRepository.java
@@ -1,11 +1,12 @@
 package com.enus.newsletter.db.repository.imp;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.enus.newsletter.model.dto.ChatMessage;
 
+
 @Repository
-public interface IRedisRepository extends JpaRepository<ChatMessage, String> {
+public interface IRedisRepository extends CrudRepository<ChatMessage, String> {
     
 }
