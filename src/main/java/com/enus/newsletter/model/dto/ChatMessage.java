375af1751d4fc80d116566e4c21d3c33d685b4ad
rlee <rlee@lscm.hk>
2025-03-17 16:06:51 +0800

    update dto. timestampe will be generated in server

--- src/main/java/com/enus/newsletter/model/dto/ChatMessage.java
+++ src/main/java/com/enus/newsletter/model/dto/ChatMessage.java
@@ -2,9 +2,6 @@ package com.enus.newsletter.model.dto;
 
 import java.io.Serializable;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@RedisHash("ChatMessage")
 public class ChatMessage implements Serializable {
-    @Id
-    private String id;
     private String sender;
     private String content;
-    private String timestamp;
 }
