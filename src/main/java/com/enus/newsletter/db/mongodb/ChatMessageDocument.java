bd85878ea191aa51dcb1e70248f9f047f229ce66
rlee <rlee@lscm.hk>
2025-03-17 16:06:17 +0800

    update document and add indexing

--- src/main/java/com/enus/newsletter/db/mongodb/ChatMessageDocument.java
+++ src/main/java/com/enus/newsletter/db/mongodb/ChatMessageDocument.java
@@ -8,18 +8,28 @@ package com.enus.newsletter.db.mongodb;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Builder;
+import lombok.Data;
 
 /**
  *
  * @author idohyeon
  */
 @Document(collection = "chat_messages")
+@CompoundIndex(name = "sessionId_timestamp", def = "{'sessionId': 1, 'timestamp': 1}")
+@Builder
+@Data
 public class ChatMessageDocument {
     @Id
     private String id;
+    @Indexed
+    private String sessionId;
     private String sender;
     private String content;
+    @Indexed
     private LocalDateTime timestamp;
-    private String converstaionId;
 }
