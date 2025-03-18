f84ef258289cc3cc84a2ccc9d98f214ceddbd134
rlee <rlee@lscm.hk>
2025-03-18 22:47:56 +0800

    add pub/sub path

--- src/main/java/com/enus/newsletter/controller/ChatController.java
+++ src/main/java/com/enus/newsletter/controller/ChatController.java
@@ -1,8 +1,12 @@
 package com.enus.newsletter.controller;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import com.enus.newsletter.model.dto.ChatMessage;
@@ -14,9 +18,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "CHAT_CONTROLLER")
 public class ChatController {
     private ChatService chatService;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    public ChatController(ChatService chatService) {
+
+    public ChatController(ChatService chatService, SimpMessagingTemplate messagingTemplate) {
         this.chatService = chatService;
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/init")
+    @SendTo("/topic/init")
+    public void init() {
+        log.info("Init");
+        String chatSessionId = UUID.randomUUID().toString();
+        // create object to send to client
+        HashMap<String, String> map = new HashMap<>();
+        map.put("chatSessionId", chatSessionId);
+        messagingTemplate.convertAndSend("/topic/init", map);
     }
 
     @MessageMapping("/chat")
@@ -25,5 +43,21 @@ public class ChatController {
         log.info("Received message: {}", message);
         ChatMessage c = chatService.saveChatHistory(message);
         return c;
+    }
+
+    @MessageMapping("/chat/client/{chatId}")
+    @SendTo("/topic/server/{chatId}")
+    public ChatMessage handleClientMessage(@Payload ChatMessage message) {
+        log.info("Received message: {}", message);
+        chatService.saveChatHistory(message);
+        return message;
+    }
+
+    @MessageMapping("/chat/server/{chatId}")
+    @SendTo("/topic/client/{chatId}")
+    public ChatMessage handleServerMessage(@Payload ChatMessage message) {
+        log.info("Received message: {}", message);
+        chatService.saveChatHistory(message);
+        return message;
     }
 }
--- src/main/java/com/enus/newsletter/model/dto/ChatMessage.java
+++ src/main/java/com/enus/newsletter/model/dto/ChatMessage.java
@@ -3,14 +3,18 @@ package com.enus.newsletter.model.dto;
 import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ChatMessage implements Serializable {
-    private String sessionId;
+    // Chat Message sent from publisher to subscriber
+
+    private String chatId;
     private String sender;
     private String content;
 }
