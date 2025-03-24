c795f15f4fb9f947208fcf27fbbe3c3d0e4125e2
rlee <rlee@lscm.hk>
2025-03-24 16:48:03 +0800

    add function to save tts for a chat session

--- src/main/java/com/enus/newsletter/service/TtsService.java
+++ src/main/java/com/enus/newsletter/service/TtsService.java
@@ -7,6 +7,12 @@ package com.enus.newsletter.service;
 
 import org.springframework.stereotype.Service;
 
+import com.enus.newsletter.db.entity.TtsEntity;
+import com.enus.newsletter.db.repository.TtsRepository;
+import com.enus.newsletter.enums.DetailLevel;
+import com.enus.newsletter.enums.ReadSpeed;
+import com.enus.newsletter.model.dto.TtsDetails;
+import com.enus.newsletter.model.dto.TtsDto;
 import com.enus.newsletter.model.request.tts.TtsRequest;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +25,32 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class TtsService {
 
-    public void saveTTSConfig(TtsRequest dto) {
+    private final TtsRepository repository;
 
+    public TtsService(TtsRepository repository) {
+        this.repository = repository;
+    }
+
+    public TtsDto saveTTSConfig(TtsRequest dto) {
+        log.info("Saving TTS config: {}", dto);
+        TtsEntity entity = repository.save(dto);
+
+        int wordsPerBriefing = DetailLevel.getValueByName(dto.getDetailLevel());
+        int estimatedSeconds = (int) ((wordsPerBriefing * entity.getBriefingCount()) / (5 * ReadSpeed.getValueByName(dto.getReadSpeed())));
+
+        TtsDetails details = TtsDetails.builder()
+            .estimatedSeconds(estimatedSeconds)
+            .wordsPerBriefing(wordsPerBriefing)
+            .wordCount(wordsPerBriefing * entity.getBriefingCount())
+            .build();
+
+        TtsDto ttsDto = TtsDto.builder()
+            .chatId(entity.getChatSession().getChatId())
+            .readSpeed(ReadSpeed.getValueByName(dto.getReadSpeed()))
+            .detailLevel(DetailLevel.getValueByName(dto.getDetailLevel()))
+            .briefingCount(entity.getBriefingCount())
+            .ttsDetails(details)
+            .build();
+        return ttsDto;
     }
 }
