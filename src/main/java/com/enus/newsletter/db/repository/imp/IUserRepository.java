1959092aeeeec48fd1244addd18d1134da3958b1
eqfwcev123 <dohyeonee95@hotmail.com>
2025-03-25 06:46:12 +0800

    change findUserByIds return type to be optional

--- src/main/java/com/enus/newsletter/db/repository/imp/IUserRepository.java
+++ src/main/java/com/enus/newsletter/db/repository/imp/IUserRepository.java
@@ -1,18 +1,20 @@
 package com.enus.newsletter.db.repository.imp;
 
-import com.enus.newsletter.db.entity.UserEntity;
-import jakarta.validation.constraints.Email;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.enus.newsletter.db.entity.UserEntity;
+
+import jakarta.validation.constraints.Email;
 
 
 @Repository
 public interface IUserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-    UserEntity findById(long id);
+    Optional<UserEntity> findById(long id);
     Optional<UserEntity> findByUsername(String username);
 
     Optional<UserEntity> findByEmail(@Email String email);
