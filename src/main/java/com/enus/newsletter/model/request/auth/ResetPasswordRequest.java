741b5a63c9e22d1e048adf7e6480f56d67cee1df
rlee <rlee@lscm.hk>
2025-03-27 17:34:24 +0800

    remove email code as verification code is code sent to email

--- src/main/java/com/enus/newsletter/model/request/auth/ResetPasswordRequest.java
+++ src/main/java/com/enus/newsletter/model/request/auth/ResetPasswordRequest.java
@@ -20,12 +20,7 @@ public class ResetPasswordRequest {
     private String newPassword;
 
     @NotBlank(message = "verification code required")
-    @NotBlank(message = "Please check your email for the verification code")
     @JsonProperty("verificationCode")
     private String verificationCode;
-
-    @NotBlank(message = "email code required")
-    @JsonProperty("emailCode")
-    private String emailCode;
 
 }
