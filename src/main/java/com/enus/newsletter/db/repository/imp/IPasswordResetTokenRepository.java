24704918967e5c9bb643dbec7a6a3185474eecfb
rlee <rlee@lscm.hk>
2025-03-27 17:35:08 +0800

    update query, only check for code sent to email

--- src/main/java/com/enus/newsletter/db/repository/imp/IPasswordResetTokenRepository.java
+++ src/main/java/com/enus/newsletter/db/repository/imp/IPasswordResetTokenRepository.java
@@ -1,19 +1,24 @@
 package com.enus.newsletter.db.repository.imp;
 
-import com.enus.newsletter.db.entity.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.enus.newsletter.db.entity.PasswordResetTokenEntity;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
-public interface IPasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+public interface IPasswordResetTokenRepository extends JpaRepository<PasswordResetTokenEntity, Long> {
     @Query(
-            "SELECT prt FROM PasswordResetToken prt WHERE prt.responseCode = :responseCode AND prt.emailCode = :emailCode AND prt.expiresAt > :now"
+        """
+        SELECT prt 
+        FROM PasswordResetTokenEntity prt 
+        WHERE prt.code = :code AND prt.expiresAt > :now                
+        """
     )
-    Optional<PasswordResetToken> findValidTokenByEmailCode(
-            String responseCode, String emailCode, LocalDateTime now
+    Optional<PasswordResetTokenEntity> findValidTokenByEmailCode(
+            String code, LocalDateTime now
     );
 }
