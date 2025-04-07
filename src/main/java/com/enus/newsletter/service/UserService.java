f918bbbb195e221ea235d69f9d7e2fba040c6a37
rlee <rlee@lscm.hk>
2025-04-07 17:24:40 +0800

    get user information

--- src/main/java/com/enus/newsletter/service/UserService.java
+++ src/main/java/com/enus/newsletter/service/UserService.java
@@ -4,10 +4,32 @@ import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import com.enus.newsletter.db.entity.UserEntity;
+import com.enus.newsletter.db.repository.UserRepository;
+import com.enus.newsletter.model.dto.UserDTO;
+
 
 @Service
 @Transactional
 @Slf4j
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDTO getUser(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email);
+        return UserDTO.builder()
+            .userId(userEntity.getId())
+            .firstName(userEntity.getFirstName())
+            .lastName(userEntity.getLastName())
+            .username(userEntity.getUsername())
+            .email(userEntity.getEmail())
+            .isOauthUser(userEntity.isOauthUser())
+            .build();
+    }
 
 }
