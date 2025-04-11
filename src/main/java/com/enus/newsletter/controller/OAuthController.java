d3e1dfc45d37ad29b8c43da2353da334a469a40d
rlee <rlee@lscm.hk>
2025-04-11 16:02:51 +0800

    redirect oauth users to success to handle user creation and token generation

--- src/main/java/com/enus/newsletter/controller/OAuthController.java
+++ src/main/java/com/enus/newsletter/controller/OAuthController.java
@@ -1,7 +1,13 @@
 package com.enus.newsletter.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.enus.newsletter.model.response.Token;
+import com.enus.newsletter.service.AuthService;
+import com.enus.newsletter.service.JwtService;
+import com.enus.newsletter.system.GeneralServerResponse;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -17,6 +24,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 @RequestMapping("/api/oauth")
 public class OAuthController {
+
+    private final JwtService jwtService;
+    private final AuthService authService;
+
+    public OAuthController(JwtService jwtService, AuthService authService) {
+        this.jwtService = jwtService;
+        this.authService = authService;
+    }
 
     @GetMapping("/google")
     public void googleAuth(HttpServletResponse response) throws IOException {
@@ -37,8 +52,26 @@ public class OAuthController {
     }
 
     @GetMapping("/success")
-    public void success(HttpServletResponse response) throws IOException {
+    public ResponseEntity<GeneralServerResponse<Token>> success(HttpServletResponse response, @RequestParam String state) throws IOException {
         log.info("OAuth2 login success");
-        response.sendRedirect("/api/auth/oauth/success");
+        String email = jwtService.extractEmail(state);
+        log.info("\t\t Email is : {}", email);   
+        authService.createUserByEmail(email);
+        String accessToken = jwtService.generateTokenByEmail(email);
+        String refreshToken = jwtService.generateRefreshTokenByEmail(email);
+
+        Token token = Token.builder()
+            .accessToken(accessToken)
+            .refreshToken(refreshToken)
+            .build();
+
+        GeneralServerResponse<Token> responseBody = new GeneralServerResponse<>(
+            false,
+            "Successfully authenticated user",
+            200,
+            token
+        );
+
+        return ResponseEntity.ok(responseBody);
     }
 }
