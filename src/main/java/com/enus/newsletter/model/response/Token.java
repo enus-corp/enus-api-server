436e01628ddcbc9bc4ff0f5d72e5fd6c78092d14
rlee <rlee@lscm.hk>
2025-04-17 16:14:44 +0800

    add conversion methods

--- src/main/java/com/enus/newsletter/model/response/Token.java
+++ src/main/java/com/enus/newsletter/model/response/Token.java
@@ -1,6 +1,9 @@
 package com.enus.newsletter.model.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,4 +15,23 @@ public class Token {
 
     @JsonProperty("refreshToken")
     private String refreshToken;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public String toJson() {
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to convert Token to JSON", e);
+        }
+    }
+
+    public static Token fromJson(String json) {
+        try {
+            return objectMapper.readValue(json, Token.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to convert JSON to Token", e);
+        }
+    }
+    
 }
