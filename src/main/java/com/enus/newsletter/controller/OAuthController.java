b2cb3f01370333914917f89042e07e840b8618a3
eqfwcev123 <dohyeonee95@hotmail.com>
2025-04-19 23:23:59 +0800

    fixed wrong import

--- src/main/java/com/enus/newsletter/controller/OAuthController.java
+++ src/main/java/com/enus/newsletter/controller/OAuthController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,6 @@ import com.enus.newsletter.model.response.Token;
 import com.enus.newsletter.service.TokenService;
 import com.enus.newsletter.system.GeneralServerResponse;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
