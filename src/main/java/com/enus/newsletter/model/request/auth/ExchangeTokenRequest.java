9adee9c67230485b0e5595aa0562ee467d9bcadc
eqfwcev123 <dohyeonee95@hotmail.com>
2025-04-19 23:25:03 +0800

    update token request

--- src/main/java/com/enus/newsletter/model/request/auth/ExchangeTokenRequest.java
+++ src/main/java/com/enus/newsletter/model/request/auth/ExchangeTokenRequest.java
@@ -1,10 +1,15 @@
 package com.enus.newsletter.model.request.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ExchangeTokenRequest {
-    @NotBlank
+    @NotBlank(message = "Temp Token is required")
+    @JsonProperty("tempToken")
     private String tempToken;
 }
