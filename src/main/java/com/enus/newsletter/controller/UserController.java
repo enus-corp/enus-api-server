2849e64aed5e324baafd4edbe2927cd58948a2b3
eqfwcev123 <dohyeonee95@hotmail.com>
2025-04-21 23:07:22 +0800

    add update user api

--- src/main/java/com/enus/newsletter/controller/UserController.java
+++ src/main/java/com/enus/newsletter/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.enus.newsletter.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.enus.newsletter.interfaces.CustomUserDetailsImpl;
 import com.enus.newsletter.model.dto.UserDTO;
+import com.enus.newsletter.model.request.user.UpdateUserRequest;
 import com.enus.newsletter.service.UserService;
 import com.enus.newsletter.system.GeneralServerResponse;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+
 
 @Slf4j(topic = "UserController")
 @Tag(name="User", description = "User API")
@@ -29,8 +39,12 @@ public class UserController {
         UserDTO user = userService.getUser(currentUser.getEmail());
         return ResponseEntity.ok(new GeneralServerResponse<>(false, null, 0, user));
     }
-    @PostMapping("/updateUser")
-    public void updateUser() {}
+    @PutMapping("/update")
+    public ResponseEntity<GeneralServerResponse<Void>> updateUser(@AuthenticationPrincipal CustomUserDetailsImpl currentUser, @Valid @RequestBody UpdateUserRequest body) {
+        log.info("[UserController][UpdateUser] Updating user info");
+        userService.updateUser(currentUser.getEmail(), body);
+        return ResponseEntity.ok(new GeneralServerResponse<>(false, "Successfully updated user information", 0, null));
+    }
 
     @DeleteMapping("/delete")
     public void delete() {}
