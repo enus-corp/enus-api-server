b374e116132e71712e800178c80a2776cb982c81
eqfwcev123 <dohyeonee95@hotmail.com>
2025-04-21 23:06:57 +0800

    update allowed origins

--- src/main/java/com/enus/newsletter/config/SecurityConfig.java
+++ src/main/java/com/enus/newsletter/config/SecurityConfig.java
@@ -116,10 +116,11 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST" , "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
