805b6f2c5f295d8f66f99ef09a26c375eb0ca285
rlee <rlee@lscm.hk>
2025-04-22 09:52:28 +0800

    move authentication manager bean to security config

--- src/main/java/com/enus/newsletter/config/ApplicationConfig.java
+++ src/main/java/com/enus/newsletter/config/ApplicationConfig.java
@@ -2,17 +2,11 @@ package com.enus.newsletter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
